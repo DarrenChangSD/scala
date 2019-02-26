4f6f1a529a5c7b9eb78e36acdad66968ff6d9d19
DarrenChangSD <43389229+DarrenChangSD@users.noreply.github.com>
2019-02-26 13:57:25 -0800

    Done 2/26/2019.

---
+++ Stack.scala
@@ -0,0 +1,39 @@
+package linkedlist.adt
+
+class Stack[T] {
+  private case class Node(data: T, next: Node)
+  private var top: Node = null
+
+  def push(obj: T): Unit = {
+    top = Node(obj, top)
+  }
+
+  def pop() : T = {
+    assert(!isEmpty)
+    val ret = top.data
+    top = top.next
+    ret
+  }
+
+  def peek(): T = {
+    top.data
+  }
+
+  def isEmpty(): Boolean = {
+    top == null
+  }
+
+  def printStack(): Unit = {
+    var traver = top
+    print("[")
+    while(traver != null) {
+      if(traver.next == null) {
+        print(traver.data)
+      } else {
+        print(traver.data + ", ")
+      }
+      traver = traver.next
+    }
+    print("]")
+  }
+}
---
+++ StackTest.scala
@@ -0,0 +1,18 @@
+import linkedlist.adt.Stack
+
+object StackTest extends App {
+  val testStack = new Stack[Int]
+
+  testStack.push(1)
+  testStack.push(2)
+  testStack.push(3)
+  testStack.printStack()
+
+  println()
+
+  testStack.pop()
+  testStack.printStack()
+
+  println()
+
+}
